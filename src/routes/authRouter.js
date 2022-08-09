e72fc226e7c2baf3a643bb17cb204db3e79bd2cd
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-09 20:35:38 -0300

    feat: implement post route for signup

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
+import validateSchema from "./../middlewares/validations.js";
+import checkIfEmailExists from "./../middlewares/auth/checkIfEmailExists.js";
+import { insertUser } from "./../repositories/authRepository.js";
 
 const authRouter = Router();
 
-authRouter.post("/signup", )
+authRouter.post("/signup", validateSchema("signup"), checkIfEmailExists, insertUser);
 
 export default authRouter;
