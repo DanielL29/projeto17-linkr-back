b12346961458ba2e649ac357ecad064d1c692842
Pedro H <driverique@gmail.com>
2022-08-11 21:59:02 -0300

    Fixing: get

--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -1,22 +1,36 @@
-import connection from '../database/db.js'
+import connection from "../database/db.js";
 
-async function insertPost(url, description = '', urlImage, urlDescription, urlTitle, userId) {
-    return connection.query(`
+async function insertPost(
+  url,
+  description = "",
+  urlImage,
+  urlDescription,
+  urlTitle,
+  userId
+) {
+  return connection.query(
+    `
         INSERT INTO posts 
             (url, description, "urlImage", "urlDescription", "urlTitle", "ownerId")
             VALUES ($1, $2, $3, $4, $5, $6) 
         RETURNING id
-    `, [url, description, urlImage, urlDescription, urlTitle, userId])
+    `,
+    [url, description, urlImage, urlDescription, urlTitle, userId]
+  );
 }
 
 async function insertPostHashtags(postId, hashtagId) {
-    connection.query('INSERT INTO "postHashtags" ("postId", "hashtagId") VALUES ($1, $2)', [postId, hashtagId])
+  connection.query(
+    'INSERT INTO "postHashtags" ("postId", "hashtagId") VALUES ($1, $2)',
+    [postId, hashtagId]
+  );
 }
 
 async function selectPosts(hashtag, username) {
-    if (hashtag) {
-        return connection.query(`
-            SELECT p.*, u.username 
+  if (hashtag) {
+    return connection.query(
+      `
+            SELECT p.*, u.username, u."pictureUrl" 
             FROM "postHashtags" ph
             JOIN hashtags h ON h.id = ph."hashtagId"
             JOIN posts p ON p.id = ph."postId"
@@ -24,18 +38,29 @@ async function selectPosts(hashtag, username) {
             WHERE h.name = $1
             ORDER BY p.id DESC
             LIMIT 20
-        `, [hashtag])
-    } else if(username) {
-
-    } else {
-        return connection.query(`
+        `,
+      [hashtag]
+    );
+  } else if (username) {
+    return connection.query(
+      `
+      SELECT p.*, u.username, u."pictureUrl"
+      FROM posts p 
+      JOIN users u ON p."ownerId" = u.id
+      WHERE u.id = $1
+      ORDER BY p.id DESC
+  `,
+      [username]
+    );
+  } else {
+    return connection.query(`
             SELECT p.*, u.username
             FROM posts p 
             JOIN users u ON p."ownerId" = u.id 
             ORDER BY p.id DESC 
             LIMIT 20
-        `)
-    }
+        `);
+  }
 }
 
-export { insertPost, insertPostHashtags, selectPosts }
+export { insertPost, insertPostHashtags, selectPosts };
