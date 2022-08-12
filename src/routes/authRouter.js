aceeb9636c01f7fc9826cf35dfa338b26c293b65
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-12 11:50:54 -0300

    fix: route name

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -7,7 +7,7 @@ import checkCredentials from "../middlewares/auth/checkCredentials.js";
 const authRouter = Router();
 
 authRouter.post("/signup", validateSchema("signup"), checkIfEmailExists, signup);
-authRouter.post("signin", validateSchema("signin"), checkCredentials, signin);
+authRouter.post("/signin", validateSchema("signin"), checkCredentials, signin);
 authRouter.get("/search", searchUsers)
 
 export default authRouter;
