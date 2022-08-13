166377101bdb4a8a64a2b535eda79040de21efb1
Daniel Lucas Ederli <danielnatsufire@gmail.com>
2022-08-12 21:17:02 -0300

    fix: putting token in routes

--- src/routes/authRouter.js
+++ src/routes/authRouter.js
@@ -3,12 +3,13 @@ import validateSchema from "./../middlewares/validations/validateSchema.js";
 import checkIfEmailExists from "./../middlewares/auth/checkIfEmailExists.js";
 import { signup, searchUsers, signin, getUser } from "./../controllers/authController.js";
 import checkCredentials from "../middlewares/auth/checkCredentials.js";
+import validateJwtToken from "../middlewares/auth/validateJwtToken.js";
 
 const authRouter = Router();
 
 authRouter.post("/signup", validateSchema("signup"), checkIfEmailExists, signup);
 authRouter.post("/signin", validateSchema("signin"), checkCredentials, signin);
-authRouter.get("/search", searchUsers)
-authRouter.get("/users/:userId", getUser)
+authRouter.get("/search", validateJwtToken, searchUsers)
+authRouter.get("/users/:userId", validateJwtToken, getUser)
 
 export default authRouter;
