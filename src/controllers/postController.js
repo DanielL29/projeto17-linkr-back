7975439b28ba1376ff5e74233aeeea81eac38905
hackintosh <driverique@gmail.com>
2022-08-13 14:53:59 -0300

    Feat: Delete from postHashtags table

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -84,9 +84,10 @@ async function updatePostByUser(req, res) {
 async function deletePostByUser(req, res) {
     const { postId } = req.params
     const { user } = res.locals
-    
-    try {
-        await deletePost(postId, user)
+    console.log(user)
+    try {        
+        await deletePost(postId, user) 
+        
         return res.status(202).send("Delete post successfully")
     } catch (err) { 
         console.log(err)
--- src/repositories/postRepository.js
+++ src/repositories/postRepository.js
@@ -1,35 +1,35 @@
 import connection from "../database/db.js";
 
 async function insertPost(
-    url,
-    description = "",
-    urlImage,
-    urlDescription,
-    urlTitle,
-    userId
+  url,
+  description = "",
+  urlImage,
+  urlDescription,
+  urlTitle,
+  userId
 ) {
-    return connection.query(
-        `
+  return connection.query(
+    `
         INSERT INTO posts 
             (url, description, "urlImage", "urlDescription", "urlTitle", "ownerId")
             VALUES ($1, $2, $3, $4, $5, $6) 
         RETURNING id
     `,
-        [url, description, urlImage, urlDescription, urlTitle, userId]
-    );
+    [url, description, urlImage, urlDescription, urlTitle, userId]
+  );
 }
 
 async function insertPostHashtags(postId, hashtagId) {
-    connection.query(
-        'INSERT INTO "postHashtags" ("postId", "hashtagId") VALUES ($1, $2)',
-        [postId, hashtagId]
-    );
+  connection.query(
+    'INSERT INTO "postHashtags" ("postId", "hashtagId") VALUES ($1, $2)',
+    [postId, hashtagId]
+  );
 }
 
 async function selectPosts(hashtag, username) {
-    if (hashtag) {
-        return connection.query(
-            `
+  if (hashtag) {
+    return connection.query(
+      `
             SELECT p.*, u.username, u."pictureUrl" 
             FROM "postHashtags" ph
             JOIN hashtags h ON h.id = ph."hashtagId"
@@ -39,11 +39,11 @@ async function selectPosts(hashtag, username) {
             ORDER BY p.id DESC
             LIMIT 20
         `,
-            [hashtag]
-        );
-    } else if (username) {
-        return connection.query(
-            `
+      [hashtag]
+    );
+  } else if (username) {
+    return connection.query(
+      `
             SELECT p.*, u.username, u."pictureUrl"
             FROM posts p 
             JOIN users u ON p."ownerId" = u.id
@@ -51,50 +51,78 @@ async function selectPosts(hashtag, username) {
             ORDER BY p.id DESC
             LIMIT 20
         `,
-            [username]
-        );
-    } else {
-        return connection.query(`
+      [username]
+    );
+  } else {
+    return connection.query(`
             SELECT p.*, u.username, u."pictureUrl"
             FROM posts p 
             JOIN users u ON p."ownerId" = u.id 
             ORDER BY p.id DESC 
             LIMIT 20
         `);
-    }
+  }
 }
 
-async function updatePost(description, postId, userId){
-
-  return connection.query(`
+async function updatePost(description, postId, userId) {
+  return connection.query(
+    `
     UPDATE posts p
     SET description = $1 
     WHERE p.id = $2 AND p."ownerId" = $3
-  `, [description, postId, userId]);
-
+  `,
+    [description, postId, userId]
+  );
 }
 
 async function selectPostHashtags(postId, hashtagId) {
-    return connection.query(`
+  return connection.query(
+    `
         SELECT * 
         FROM "postHashtags" ph
         WHERE ph."postId" = $1 
         AND ph."hashtagId" = $2 
-    `, [postId, hashtagId])
+    `,
+    [postId, hashtagId]
+  );
 }
 
 async function selectPost(postId) {
-    return connection.query('SELECT description FROM posts WHERE id = $1', [postId])
+  return connection.query("SELECT description FROM posts WHERE id = $1", [
+    postId,
+  ]);
 }
 
 async function deletePostHashtags(postId, hashtagId) {
-    connection.query('DELETE FROM "postHashtags" WHERE "postId" = $1 AND "hashtagId" = $2', [postId, hashtagId])
+  connection.query(
+    'DELETE FROM "postHashtags" WHERE "postId" = $1 AND "hashtagId" = $2',
+    [postId, hashtagId]
+  );
 }
 
 async function deletePost(postId, userId) {
-  return connection.query(`
-  DELETE FROM posts WHERE id = $1 AND "ownerId" = $2
-`, [postId, userId]);
+
+  await connection.query(
+    `
+  DELETE FROM posts
+  WHERE id = $1 AND "ownerId" = $2
+`,
+    [postId, userId]
+  );
+
+  await connection.query(
+    `DELETE FROM "postHashtags" WHERE "postId" = $1`,
+    [postId]
+  );
 }
 
-export { insertPost, insertPostHashtags, selectPosts, updatePost, deletePost, selectPostHashtags, selectPost, deletePostHashtags };
+export {
+  insertPost,
+  insertPostHashtags,
+  selectPosts,
+  updatePost,
+  deletePost,
+  selectPostHashtags,
+  selectPost,
+  deletePostHashtags,
+};
