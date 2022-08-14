669c851dce9013036f8b3fd38ee7525759fde25f
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-13 23:03:56 -0300

    feat: implement like feature

--- src/routes/likeRouter.js
+++ src/routes/likeRouter.js
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import { dislike, getLikes, like } from "../controllers/likeController.js";
+import validateJwtToken from "./../middlewares/auth/validateJwtToken.js";
 
 const likeRouter = Router();
 
-likeRouter.post("/like", );
+likeRouter.post("/like", validateJwtToken, like);
+likeRouter.delete("/dislike", validateJwtToken, dislike);
+likeRouter.get("/likes/:id", validateJwtToken, getLikes);
 
 export default likeRouter;
