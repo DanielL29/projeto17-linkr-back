514367213dff4c32340b91610324e1bf72b31a8f
Juan Barezzi <juan.barezzi@gmail.com>
2022-08-18 15:35:06 -0300

    feat: implement router to get new posts

--- src/routes/postRouter.js
+++ src/routes/postRouter.js
@@ -1,5 +1,5 @@
 import { Router } from 'express'
-import { getPosts, publishPost, deletePostByUser, updatePostByUser} from '../controllers/postController.js'
+import { getPosts, publishPost, deletePostByUser, updatePostByUser, updateNumberOfPosts} from '../controllers/postController.js'
 import validateJwtToken from '../middlewares/auth/validateJwtToken.js'
 import urlMetadatas from '../middlewares/urlMetadatas.js'
 import validateSchema from '../middlewares/validations/validateSchema.js'
@@ -11,6 +11,6 @@ postRouter.post('/posts', validateJwtToken, validateSchema('post'), urlMetadatas
 postRouter.get('/posts', validateJwtToken, getPosts);
 postRouter.patch('/posts/:postId', validateJwtToken, verifyIfHaveHashtags, updatePostByUser);
 postRouter.delete('/posts/:postId', validateJwtToken, deletePostByUser);
-postRouter.get("/new-posts/:postId", validateJwtToken, ); 
+postRouter.get("/new-posts/:postId", validateJwtToken, updateNumberOfPosts); 
 
 export default postRouter
